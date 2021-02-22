284f20fb956b311674a4ad364ee31ed27e8e5e4e
Ryan Pinheiro <gabrielrayn024@gmail.com>
2021-02-22 17:51:39 -0300

    Projeto atualizado atividade 1

--- Atividade1.js
+++ Atividade1.js
@@ -1,9 +1,42 @@
-lets Nome1 = 'Ola Mundo'
+// Atividade 1
+
+function nome(nome,ola, ponto){
+    return ola + nome + ponto
+}
+var mostrar = nome('Gabriel ', 'Ola, ','!')
+
+console.log(mostrar)
 
 
-function nome(){
-return  Nome1
 
+
+
+// Atividade 2
+function idadeEmDia(idade){
+return  idade * 365
 }
 
-console.log(nome)
+var idade = idadeEmDia(17) // Passe a idade aqui
+console.log("Você viveu " + idade + ' dias');
+
+// Atividade 3
+function calcularSalario(hora,salario){
+    return hora * salario
+}
+
+var ValorRecebido = calcularSalario(80,12.50)
+
+console.log("Você rebera " + ValorRecebido + "R$")
+
+//atividade 4
+function Mes(meses){
+   var meses = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','desembro'];
+ 
+for(i = 1, i++, i >= 12){
+    return meses
+}
+  } 
+
+
+var escolhaOmes = Mes(1)
+console.log(escolhaOmes)
