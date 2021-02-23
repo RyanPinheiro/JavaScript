197b9f212da9b6de84ab21bcc8210fe1ab5cb715
Ryan Pinheiro <gabrielrayn024@gmail.com>
2021-02-23 09:18:02 -0300

    Uma função adicionada

--- Atividade1.js
+++ Atividade1.js
@@ -30,7 +30,8 @@ console.log("Você rebera " + ValorRecebido + "R$")
 
 //atividade 4
 //Não terminado
-function Mes(meses){
+/*
+ function Mes(meses){
    var meses = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','desembro'];
  
 for(i = 1, i++, i >= 12){
@@ -41,3 +42,19 @@ for(i = 1, i++, i >= 12){
 
 var escolhaOmes = Mes(1)
 console.log(escolhaOmes)
+*/
+
+function Numero(numero1, numero2){
+     if(numero1 > numero2){
+        console.log("O numero " + numero1 + " e maior quer " + numero2);
+    }else if(numero1 == numero2){
+        console.log("O numero " + numero1 + " e igual " +  numero2 );
+    }else{
+        console.log(" o numero " + numero1 + " e menor que " + numero2)
+    }
+
+   
+}
+var resultados = Numero(400,4)
+
+console.log(resultados)
